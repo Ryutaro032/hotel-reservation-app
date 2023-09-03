dd4ffafc15cd29fcbf814b583f02c6d829eb730d
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2023-09-03 13:04:48 +0900

    userのバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable #:validatable
   
   mount_uploader :icon_image, ImageUploader
 
   has_many :rooms, dependent: :destroy
   has_many :reservations, dependent: :destroy
 
-  validates :name,                  presence: true
-  validates :email,                 presence: true, uniqueness: true
-  validates :password,              confirmation: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
 end
